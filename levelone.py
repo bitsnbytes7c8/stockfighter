29b7356629999f527d7f947c9978e756e25da883
Arjun <arjun.sn7c8@gmail.com>
2016-08-14 05:01:19 +0530

    Refactoring levelone

--- levelone.py
+++ levelone.py
@@ -1,10 +1,14 @@
 from stockFighterAccessor import StockFighterAccessor;
 from order import Order;
 
-venue = raw_input("Enter the venue: ");
-stock = raw_input("Enter the stock symbol: ");
-account = raw_input("Enter the account: ");
+def main():
+    venue = raw_input("Enter the venue: ");
+    stock = raw_input("Enter the stock symbol: ");
+    account = raw_input("Enter the account: ");
 
-marketOrder = Order(venue, stock, 10000, 100, "buy", "market")
-accessor =  StockFighterAccessor(account);
-print accessor.placeOrder(marketOrder).text;
+    marketOrder = Order(venue, stock, 10000, 100, "buy", "market")
+    accessor =  StockFighterAccessor(account);
+    print accessor.placeOrder(marketOrder).text;
+
+if __name__=="__main__":
+    main()
